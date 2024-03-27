99d82fda8e328b4004f5c21ff78e361d5a73fa7b
Giordano Martín Díaz Merino <diazgiordano@gmail.com>
2024-03-27 10:58:54 -0500

    Finish coding challenge using TDD

--- memory_key_value_store.rb
+++ memory_key_value_store.rb
@@ -1,14 +1,21 @@
 class MemoryKeyValueStore
-
   def initialize
+    @store = {}
+  end
+  
+  def set(key, value)
+    @store[key] = value
+    "OK"
   end
 
-  def get
+  def get(key)
+    return "Error: value is not a string" unless key.is_a? String
+    @store[key]
   end
 
-  def set
-  end
-
-  def keys
+  def keys(pattern = nil)
+    return @store.keys unless pattern
+    regex = Regexp.new(pattern)
+    @store.keys.select { |key| key.match(pattern) }
   end
 end
--- spec/memory_key_value_store_spec.rb
+++ spec/memory_key_value_store_spec.rb
@@ -6,17 +6,25 @@ describe "MemoryKeyValueStore" do
   describe "#get" do
     context "with a matching key"do
       it "gets the value of key" do
+        store = MemoryKeyValueStore.new
+        store.set("zapato", "De cuero")
+        expect(store.get("zapato")).to eq("De cuero")
       end
     end
 
     context "without a matching key" do
       it "returns the special value nil" do
+        store = MemoryKeyValueStore.new
+        store.set("tv", "Smart tv")
+        expect(store.get("zapato")).to be_nil
       end
-
     end
 
     context "with a non-string value" do
       it "returns an error" do
+        store = MemoryKeyValueStore.new
+        store.set("zapato", 'De cuero')
+        expect(store.get(5)).to eq("Error: value is not a string")
       end
     end
   end
@@ -24,17 +32,29 @@ describe "MemoryKeyValueStore" do
   describe "#set" do
     context "when they key does not hold a value" do
       it "sets the key to hold the string value" do
+        store = MemoryKeyValueStore.new
+        store.set("zapato", "De cuero")
+        expect(store.get("zapato")).to eq("De cuero")
       end
 
       it "returns the string 'OK'" do
+        store = MemoryKeyValueStore.new
+        expect(store.set("zapato", "De cuero")).to eq("OK")
       end
     end
 
     context "when they key already holds a value" do
       it "sets the key to hold the string value" do
+        store = MemoryKeyValueStore.new
+        store.set("zapato", "De cuero")
+        store.set("zapato", "De gamuza")
+        expect(store.get("zapato")).to eq("De gamuza")
       end
 
       it "returns the string 'OK'" do
+        store = MemoryKeyValueStore.new
+        store.set("zapato", "De cuero")
+        expect(store.set("zapato", "De gamuza")).to eq("OK")
       end
     end
   end
@@ -42,11 +62,21 @@ describe "MemoryKeyValueStore" do
   describe "#keys" do
     context "when no regex pattern is passed" do
       it "returns all keys" do
+        store = MemoryKeyValueStore.new
+        store.set("zapato", "De cuero")
+        store.set("tv", "Smart tv")
+        store.set("laptop", "Macbook")
+        expect(store.keys).to eq(["zapato", "tv", "laptop"])
       end
     end
 
     context "when a regex pattern is passed" do
       it "returns all keys matching regex pattern" do
+        store = MemoryKeyValueStore.new
+        store.set("zapato", "De cuero")
+        store.set("tv", "Smart tv")
+        store.set("laptop", "Macbook")
+        expect(store.keys(/z/)).to eq(["zapato"])
       end
     end
   end
